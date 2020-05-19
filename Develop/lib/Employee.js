39a2bbb07108d6042d24841997fb0ee96aa05dc4
Oleksandr Prykotenko <aprykotenko@gmail.com>
2020-05-18 19:57:31 -0700

    Massive push

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,31 @@
+// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+    }
+  
+  // need to return name 
+    getName() {
+      return this.name;
+    }
+  
+  // need to return ID number
+    getId() {
+      return this.id;
+    }
+  
+  // need to return email
+    getEmail() {
+      return this.email;
+    }
+  
+  // need to return role
+    getRole() {
+      return "Employee";
+    }
+  }
+  
+  // need to export to other scripts
+  module.exports = Employee;
